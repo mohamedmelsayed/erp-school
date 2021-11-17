7eda7013a8bf8ee13c882af3091b0e70ece9da54
Mohamed <mohamedosmanelsayed@yahoo.com>
2021-11-17 11:48:59 +0200

    Update __manifest__.py
    project Informations

---
+++ prof_school/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Copyright (C) 2021-TODAY Prof-Dev Integrated(<http://www.prof-dev.com>).
+
+###############################################################################
+
+{
+    'name': 'Prof-Dev School MGMT',
+    'version': '1.0',
+    'license': 'MIT',
+    'category': 'Education',
+    "sequence": 1,
+    'summary': 'Manage Students, School stages,levels, classes and fees',
+     'complexity': "easy",
+    'author': 'Prof-Dev Integrated Solutions',
+    'website': 'http://www.prof-dev.com',
+    'depends': [ 'hr'],
+    'data': [
+       
+    ]
+
+
+}
